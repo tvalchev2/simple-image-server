319a36a4091e8ee526e02c28eefe7df781cf8450
Matthias Seidel <M.Seidel@intershop.de>
2023-11-14 11:36:02 +0100

    Initial project setup.

---
+++ simple-image-server/src/native-test/java/com/intershop/hackathon/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.intershop.hackathon;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
